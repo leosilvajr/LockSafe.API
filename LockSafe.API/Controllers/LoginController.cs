5f20b5c18bb09ac43ed52c3479f32edd8d686043
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2025-01-26 23:09:45 -0300

    Criado Controller Login com criação de Token

---
+++ LockSafe.API/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using LockSafe.Application.DTOs;
+using LockSafe.Application.DTOs.LockSafe.Application.DTOs;
+using LockSafe.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace LockSafe.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public LoginController(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDto loginDto)
+        {
+            if (loginDto.UserName == "string" && loginDto.Password == "string")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, loginDto.UserName),
+                    new Claim(ClaimTypes.Role, "Admin")
+                };
+
+                // Gerar o JWT
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: _jwtSettings.Issuer,
+                    audience: _jwtSettings.Audience,
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(_jwtSettings.ExpirationMinutes),
+                    signingCredentials: creds
+                );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+                return Ok(new { Usuario = loginDto.UserName, Token = tokenString });
+            }
+
+            return Unauthorized();
+        }
+    }
+}
--- LockSafe.API/Program.cs
+++ LockSafe.API/Program.cs
@@ -1,9 +1,13 @@
 using LockSafe.Application.Services;
 using LockSafe.Application.Services.Interface;
+using LockSafe.Domain.Models;
 using LockSafe.Infra.Context;
 using LockSafe.Infra.Repositories;
 using LockSafe.Infra.Repositories.Interface;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +15,9 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configurações do JwtSettings
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
 
 // Configurar o DbContext com SQL Server
 builder.Services.AddDbContext<LockSafeContext>(options =>
@@ -24,6 +31,26 @@ builder.Services.AddScoped<IUserService, UserService>();
 // Repositórios
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
+
+// Configurações do JwtSettings
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+
+// Configurar o JWT Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings.Issuer,
+            ValidAudience = jwtSettings.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+        };
+    });
 
 
 var app = builder.Build();
@@ -40,6 +67,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Usar autenticação
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
